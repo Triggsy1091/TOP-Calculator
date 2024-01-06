13b00d760142fbc1922ae43a3ba29a2476833364
Triggsy1091 <ntriggs01@gmail.com>
2024-01-07 03:17:22 +1100

    added event listeners to buttons

--- main.js
+++ main.js
@@ -2,6 +2,26 @@
 var firstNumber;
 var secondNumber;
 var operatorSign;
+var topDisplayValue;
+var bottomDisplayValue;
+
+const buttonContainer = document.querySelector('.grid-layout');
+
+const calculatorButtons = buttonContainer.querySelectorAll('.calculator-button');
+
+calculatorButtons.forEach((button) => {
+        button.addEventListener('click', buttonPressed);
+});
+
+// Testing button presses
+function buttonPressed() {
+    var buttonPressed = Number(this.id);
+    if (isNaN(buttonPressed)) {
+        console.log('not a number');
+    } else {
+        console.log(this.id);
+    }
+};
 
 // Operator functions
 
@@ -36,3 +56,4 @@ function operate(operator, a, b) {
         divideValues(a, b);
     }
 };
+
