9e084d9dce17ee56f6b3a69c8d95e4ad4f6917c7
Triggsy1091 <ntriggs01@gmail.com>
2024-01-11 11:54:30 +1100

    Added code to populate the bottom display
    Still need to add code to populate bottom display and handle operators, as well as equals sign and other code related.

--- main.js
+++ main.js
@@ -1,59 +1,78 @@
 // Variables
 var firstNumber;
 var secondNumber;
-var operatorSign;
-var topDisplayValue;
-var bottomDisplayValue;
+var operatorSignSelected;
+var operatorSigns = ["+", "-", "/", "X"];
+var operatorSelected = false;
+var topDisplayValue = "";
+var bottomDisplayValue = "";
 
-const buttonContainer = document.querySelector('.grid-layout');
+const buttonContainer = document.querySelector(".grid-layout");
 
-const calculatorButtons = buttonContainer.querySelectorAll('.calculator-button');
+const calculatorButtons =
+  buttonContainer.querySelectorAll(".calculator-button");
 
 calculatorButtons.forEach((button) => {
-        button.addEventListener('click', buttonPressed);
+  button.addEventListener("click", buttonPressed);
 });
 
 // Testing button presses
 function buttonPressed() {
-    var buttonPressed = Number(this.id);
-    if (isNaN(buttonPressed)) {
-        console.log('not a number');
-    } else {
-        console.log(this.id);
-    }
-};
+  var buttonPressed = Number(this.id);
+  populateDisplay(this.id.toString())
+}
 
 // Operator functions
 
 // Addition
 function sumValues(a, b) {
-    return a + b;
-};
+  return a + b;
+}
 
 // Subtraction
 function subtractValues(a, b) {
-    return a - b;
-};
+  return a - b;
+}
 
 // Multiplication
 function multiplyValues(a, b) {
-    return a * b;
-};
+  return a * b;
+}
 
 // Division
 function divideValues(a, b) {
-    return a / b;
-};
+  return a / b;
+}
 
 function operate(operator, a, b) {
-    if (operator = '+') {
-        sumValues(a, b);
-    } else if (operator = '-') {
-        subtractValues(a, b);
-    } else if (operator = '*') {
-        multiplyValues(a, b);
-    } else if (operator = '/') {
-        divideValues(a, b);
+    if (operator === "+") {
+        return sumValues(a, b);
+    } else if (operator === "-") {
+        return subtractValues(a, b);
+    } else if (operator === "X") {
+        return multiplyValues(a, b);
+    } else if (operator === "/") {
+        return divideValues(a, b);
     }
-};
+    return NaN;
+}
 
+function populateDisplay(value) {
+    let bottomDisplay;
+
+    if (operatorSigns.some(sign => value.includes(sign))) {
+        console.log('value includes an operator sign');
+    }
+
+    if (operatorSelected === false && !topDisplayValue) {
+        populateBottomDisplay(value);
+    };
+    
+    function populateBottomDisplay(value) {
+        bottomDisplayValue += value;
+        bottomDisplay = document.querySelector("#bottom-display");
+        bottomDisplay.textContent = bottomDisplayValue;
+    };
+    
+    function populateTopDisplay(value) {}
+}
